49475f6ade2751f1bdbe065ffae1e3f6935cf01a
LaiMo2020 <laithmohammad11@gmail.com>
2020-12-02 10:04:56 +0000

    securing the views for no loggedin / admin user

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -17,7 +18,7 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
+            messages.error(request, 'Failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
