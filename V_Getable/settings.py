3bb0d122f5fe7eea5ced20ff2a7ec0bc67438ba6
LaiMo2020 <laithmohammad11@gmail.com>
2020-12-03 00:19:39 +0000

    deployment to heroku

--- V_Getable/settings.py
+++ V_Getable/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
