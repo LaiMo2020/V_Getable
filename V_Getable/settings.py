4d976d43e081510da84b86fc57562eea0b48410f
LaiMo2020 <laithmohammad11@gmail.com>
2020-12-03 16:34:10 +0000

    adding  keys to config/creating custome.py

--- V_Getable/settings.py
+++ V_Getable/settings.py
@@ -183,7 +183,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'v-getable'
-    AWS_S3_REGION_NAME = 'EU (Stockholm) eu-north-1'
+    AWS_S3_REGION_NAME = 'EU eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
