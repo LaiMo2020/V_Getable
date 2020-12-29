e2b538ed33557be860f8135279aaa0de1f3610e1
LaiMo2020 <laithmohammad11@gmail.com>
2020-12-29 19:06:09 +0000

    env set

--- V_Getable/settings.py
+++ V_Getable/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import env
+if os.path.exists('env.py'):
+    import env
 
 from pathlib import Path
 import dj_database_url
