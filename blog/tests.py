3d348ae5b44da0f3383ed521599485bf319b1cb0
LaiMo2020 <laithmohammad11@gmail.com>
2020-12-29 17:08:35 +0000

    adding testing in test.py for blog

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from blog.forms import CommentForm
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_view_all_posts(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog.html')
+
+
+class TestCommentForm(TestCase):
+
+    def test_name_is_required(self):
+        form = CommentForm({'name': 'Test name'})
+        self.assertFalse(form.is_valid())
+
+    def test_email_is_required(self):
+        form = CommentForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+    def test_comment_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
